891969ae99e61aaf186bcd146e58104b2a62b13f
manutdmohit <saudmohit@gmail.com>
2021-12-04 19:17:48 +0545

    moving data with context

--- src/screens/context/BlogContext.js
+++ src/screens/context/BlogContext.js
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 const BlogContext = createContext();
 
 export const BLogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
