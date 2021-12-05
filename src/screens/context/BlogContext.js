63a65d78b9efb8749781cdaaee3d2393ef8ab29f
manutdmohit <saudmohit@gmail.com>
2021-12-05 10:43:53 +0545

    updating with usereducer

--- src/screens/context/BlogContext.js
+++ src/screens/context/BlogContext.js
@@ -1,15 +1,19 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useReducer } from 'react';
 
 const BlogContext = createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
