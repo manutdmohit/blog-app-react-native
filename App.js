2eb0c9ecc45ef4d48c722560fb34e6f21aa0f3d2
manutdmohit <saudmohit@gmail.com>
2021-12-06 10:21:52 +0545

    import changed

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Provider } from './src/screens/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
+
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator(
