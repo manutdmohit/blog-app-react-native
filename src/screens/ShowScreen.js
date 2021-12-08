be169d97ef951052825a1c84f4b3d70fdea44922
manutdmohit <saudmohit@gmail.com>
2021-12-08 10:40:49 +0545

    the edit icon link

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
@@ -12,10 +13,25 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
 
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <EvilIcons name="pencil" size={35} style={styles.edit} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
 export default ShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  edit: {
+    marginRight: 10,
+  },
+});
