be6cf3c89fbd160a17f44c084bd509e54ecdd6db
manutdmohit <saudmohit@gmail.com>
2021-12-08 14:48:36 +0545

    intializing edit screen with Context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { useState } from 'react/cjs/react.development';
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((post) => post.id === navigation.getParam('id'));
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen -{navigation.getParam('id')} </Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        value={title}
+        style={styles.input}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        value={content}
+        style={styles.input}
+        onChangeText={(text) => setContent(text)}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
 
 export default EditScreen;
