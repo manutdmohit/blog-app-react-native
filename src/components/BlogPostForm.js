8d27cbbeaab6bdf00dcc25ea2b7168b1ba3c0371
manutdmohit <saudmohit@gmail.com>
2021-12-08 16:43:21 +0545

    customizing on submit

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   return (
@@ -19,7 +19,10 @@ const BlogPostForm = () => {
         onChangeText={(text) => setContent(text)}
       />
       <View>
-        <Button title="Save Blog Post" />
+        <Button
+          title="Save Blog Post"
+          onPress={() => onSubmit(title, content)}
+        />
       </View>
     </View>
   );
